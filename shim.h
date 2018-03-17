790db6f421b7b09695f48ea8aa312b692f9296d4
vdka <ethanjackwitz@gmail.com>
2018-03-17 13:46:21 +1000

    Added a shim for calling uc_add_hook

--- shim.h
+++ shim.h
@@ -1,3 +1,8 @@
 
- #include <unicorn/unicorn.h>
+#include <unicorn/unicorn.h>
+
+uc_err uc_hook_add_shim(uc_engine *uc, uc_hook *hh, int type, void *callback,
+        void *user_data, uint64_t begin, uint64_t end, va_list args) {
+    return uc_hook_add(uc, hh, type, callback, user_data, begin, end, args);
+}
 
